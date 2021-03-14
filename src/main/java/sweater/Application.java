3db69ef2064c9af1ec89d742c66a1132a887ed67
Божко Александр Викторович <qa_aleksvik@outlook.com>
2021-03-14 16:45:05 +0400

    Initial commit

---
+++ src/main/java/sweater/Application.java
@@ -0,0 +1,18 @@
+package sweater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/sweater/domain/Message.java
@@ -0,0 +1,4 @@
+package sweater.domain;
+
+public class Message {
+}
