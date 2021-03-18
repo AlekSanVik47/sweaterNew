a5afc61cce4fefd718d125d95399254c809f85ee
Божко Александр Викторович <qa_aleksvik@outlook.com>
2021-03-18 08:04:02 +0400

    с настройками postgres

--- src/main/java/sweater/repos/MessageRepo.java
+++ src/main/java/sweater/repos/MessageRepo.java
@@ -3,5 +3,5 @@ package sweater.repos;
 import org.springframework.data.repository.CrudRepository;
 import sweater.domain.Message;
 
-public interface MessageRepo extends CrudRepository <Message, Integer> {
+public interface MessageRepo extends CrudRepository <Message, Long> {
 }
